acc9c48fd36f7abe5d259fe66accdd78abaa1acd
jared12fg <132527713+jared12fg@users.noreply.github.com>
2023-10-17 18:41:18 -0600

    Modulos de menu
    se agregaron los modulos, que se mostraran en el menu

--- appreact/src/page/nabvar/sidebat.js
+++ appreact/src/page/nabvar/sidebat.js
@@ -4,19 +4,36 @@ export const Sidebar = [
   {
     title: "Menú principal",
     path: "/",
-    
     cName: "nav-text",
   },
   {
     title: "Ajustes",
     path: "/ajustes",
-
     cName: "nav-text",
   },
   {
     title: "Personas",
     path: "/personas",
-
+    cName: "nav-text",
+  },
+  {
+    title: "Usuarios",
+    path: "/usuario",
+    cName: "nav-text",
+  },
+  {
+    title: "Almacen",
+    path: "/almacen",
+    cName: "nav-text",
+  },
+  {
+    title: "Tienda",
+    path: "/tienda",
+    cName: "nav-text",
+  },
+  {
+    title: "Perfiles",
+    path: "/perfil",
     cName: "nav-text",
   },
   
